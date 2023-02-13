cdb458c990f2a9cd89c81789ee8a31c655d2d64b
suibowang <95519019+suibowang@users.noreply.github.com>
2023-02-13 16:09:16 +0800

    123

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,14 @@ export default defineConfig({
     assetsDir: 'assets', // 指定生成静态资源的存放路径
     minify: 'terser' // 混淆器，terser构建后文件体积更小
 },
+server: {
+  proxy: {
+    '/api': {
+      //目标地址
+      target: 'https://ceshi1.feltp.online',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, '')
+    }
+  }
+}
 })
