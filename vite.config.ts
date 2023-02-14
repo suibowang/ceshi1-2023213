a74a528900f139282c5d080ee49537a340b8fa7f
suibowang <95519019+suibowang@users.noreply.github.com>
2023-02-14 17:38:43 +0800

    阿萨

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import postcsspxtoviewport from "postcss-px-to-viewport" 
+import postcsspxtoviewport from "postcss-px-to-viewport"
 
 // https://vitejs.dev/config/
 export default defineConfig({
