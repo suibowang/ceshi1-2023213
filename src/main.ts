8f0325532595eac3487d5cd35f17341e134959d1
suibowang <95519019+suibowang@users.noreply.github.com>
2023-02-14 14:56:58 +0800

    as

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 
 import './assets/main.css'
-
+import touch from 'vue3-hand-mobile'
 const app = createApp(App)
-
+app.use(touch)
 app.use(createPinia())
 
 app.mount('#app')
---
+++ src/vue3-hand-mobile.d.ts
@@ -0,0 +1,8 @@
+declare module 'vue3-hand-mobile' {
+ 
+    type Options = {
+       
+    }
+ 
+    export default function(options: Partial<Options>):any
+}
