c32f5a1d417a6a11f82a0294108d280835225d40
suibowang <95519019+suibowang@users.noreply.github.com>
2023-02-15 10:26:08 +0800

    123

--- src/components/First.vue
+++ src/components/First.vue
@@ -1,10 +1,16 @@
 <template>
 
-<div class="abc">
-        <img src="../assets/img/FisrtImg/1-3text.png" alt="" class="text1">
-        <img src="../assets/img/FisrtImg/1.2text.png" alt="" class="text2">
-        <img src="../assets/img/FisrtImg/1-1text.png" alt="" class="text3">
-</div>
+  <div class="abc">
+    <img src="../assets/img/FisrtImg/1-3text.png" alt="" class="text1">
+    <img src="../assets/img/FisrtImg/1.2text.png" alt="" class="text2">
+    <img src="../assets/img/FisrtImg/1-1text.png" alt="" class="text3">
+    <img src="../assets/img/FisrtImg/1.4liu.png" alt="" class="liu1-1">
+    <img src="../assets/img/FisrtImg/1.4liu.png" alt="" class="liu1-2">
+    <img src="../assets/img/FisrtImg/1.5liu.png" alt="" class="liu1-3">
+    <img src="../assets/img/FisrtImg/1.5liu.png" alt="" class="liu2-1">
+    <img src="../assets/img/FisrtImg/1.5liu.png" alt="" class="liu2-2">
+    <img src="../assets/img/FisrtImg/1.5liu.png" alt="" class="liu2-3">
+  </div>
 
 </template>
 <script setup lang='ts'>
@@ -12,44 +18,137 @@ import { reactive } from 'vue';
 
 </script>
 <style scoped lang='less'>
-.abc{
-    background-image: url(../assets/img/FisrtImg/backgroud.jpg);
-    width: 100vw;
-    height:100vh;
-    background-position: center;
-    background-size: cover;
-    position: absolute;
+.abc {
+  background-image: url(../assets/img/FisrtImg/backgroud.jpg);
+  width: 100vw;
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
+  position: absolute;
 }
-.text1{
-    width: 234px;
-    height: 13px;
-    margin-left: 70px;
-    margin-top: 48px;
-    position: absolute;
-    animation:fadeInUp 0.8s ease ;
-    animation-delay: 0.3s;
-    animation-fill-mode:both;
+
+.text1 {
+  width: 234px;
+  height: 13px;
+  margin-left: 70px;
+  margin-top: 48px;
+  position: absolute;
+  animation: fadeInUp 0.8s ease;
+  animation-delay: 0.3s;
+  animation-fill-mode: both;
 }
-.text2{
-    width: 189px;
-    height: 14px;
-    margin-left: 93px;
-    margin-top: 629px;
-    position: absolute;
-    animation:fadeInUp 0.8s ease ;
-    animation-delay: 0.9s;
-    animation-fill-mode:both;
+
+.text2 {
+  width: 189px;
+  height: 14px;
+  margin-left: 93px;
+  margin-top: 629px;
+  position: absolute;
+  animation: fadeInUp 0.8s ease;
+  animation-delay: 0.9s;
+  animation-fill-mode: both;
 }
-.text3{
-    width: 205px;
-    height: 138px;
-    margin-left: 85px;
-    margin-top: 89px;
-    position: absolute;
-    animation:fadeInUp 0.8s ease ;
-    animation-delay: 0.6s;
-    animation-fill-mode:both;//动画开始前隐藏
+
+.text3 {
+  width: 205px;
+  height: 138px;
+  margin-left: 85px;
+  margin-top: 89px;
+  position: absolute;
+  animation: fadeInUp 0.8s ease;
+  animation-delay: 0.6s;
+  animation-fill-mode: both; //动画开始前隐藏
 }
+
+.liu1-1 {
+  width: 52px;
+  height: 77px;
+  margin-left: 100px;
+  margin-top: -100px;
+  position: absolute;
+  animation: liu1 3.5s linear infinite;
+  animation-delay: 1s;
+  animation-fill-mode: both;
+}
+.liu1-2 {
+  width: 52px;
+  height: 77px;
+  margin-left: 200px;
+  margin-top: -100px;
+  position: absolute;
+  animation: liu1 4s linear infinite;
+  animation-delay: 2s;
+  animation-fill-mode: both;
+}
+
+.liu1-3 {
+  width: 52px;
+  height: 77px;
+  margin-left: 300px;
+  margin-top: -100px;
+  position: absolute;
+  animation: liu1 4.9s linear infinite;
+  animation-delay: 3s;
+  animation-fill-mode: both;
+}
+.liu2-1 {
+  width: 87px;
+  height: 125px;
+  margin-left: 70px;
+  margin-top: -100px;
+  position: absolute;
+  animation: liu2 3.5s linear infinite;
+  animation-delay: 0.5s;
+  animation-fill-mode: both;
+}
+.liu2-2 {
+  width: 87px;
+  height: 125px;
+  margin-left: 150px;
+  margin-top: -100px;
+  position: absolute;
+  animation: liu2 2.8s linear infinite;
+  animation-delay: 1.5s;
+  animation-fill-mode: both;
+}
+.liu2-3 {
+  width: 87px;
+  height: 125px;
+  margin-left: 250px;
+  margin-top: -100px;
+  position: absolute;
+  animation: liu2 2.5s linear infinite;
+  animation-delay: 2.5s;
+  animation-fill-mode: both;
+}
+
+@keyframes liu1 {
+  from {
+    opacity: 1;
+    -webkit-transform: translate(100px, -100px);
+    transform: translate3d(100px, -100px);
+  }
+
+  to {
+    opacity: 1;
+    -webkit-transform: translate(-300px, 800px);
+    transform: translate(-300px, 800px);
+  }
+}
+@keyframes liu2 {
+  from {
+    opacity: 1;
+    -webkit-transform: translate(250px, -100px);
+    transform: translate3d(250px, -100px);
+  }
+
+  to {
+    opacity: 1;
+    -webkit-transform: translate(-100px, 800px);
+    transform: translate(-100px, 800px);
+  }
+}
+
 @keyframes fadeInUp {
   from {
     opacity: 0;
--- src/components/Second.vue
+++ src/components/Second.vue
@@ -1,7 +1,22 @@
 <template>
 
 <div class="abb">
-    <div>second</div>
+    <img src="../assets/img/FisrtImg/1.4liu.png" alt="" class="liu1-1">
+    <img src="../assets/img/FisrtImg/1.4liu.png" alt="" class="liu1-2">
+    <img src="../assets/img/FisrtImg/1.5liu.png" alt="" class="liu1-3">
+    <img src="../assets/img/FisrtImg/1.5liu.png" alt="" class="liu2-1">
+    <img src="../assets/img/FisrtImg/1.5liu.png" alt="" class="liu2-2">
+    <img src="../assets/img/FisrtImg/1.5liu.png" alt="" class="liu2-3">
+    <img src="../assets/img/Second/2.2.1text.png" alt="" class="text1-1">
+    <img src="../assets/img/Second/2.2.2text.png" alt="" class="text1-2">
+    <img src="../assets/img/Second/2.2.3text.png" alt="" class="text1-3">
+    <img src="../assets/img/Second/2.2.4text.png" alt="" class="text1-4">
+    <img src="../assets/img/Second/2.2.5text.png" alt="" class="text1-5">
+    <img src="../assets/img/Second/2.2.6text.png" alt="" class="text1-6">
+    <img src="../assets/img/Second/2.2.7text.png" alt="" class="text1-7">
+    <img src="../assets/img/Second/2.2.8text.png" alt="" class="text1-8">
+    <img src="../assets/img/Second/2.2.9text.png" alt="" class="text1-9">
+    <img src="../assets/img/Second/2.2.10text.png" alt="" class="text1-10">
 </div>
 
 </template>
@@ -11,7 +26,7 @@ import { reactive } from 'vue';
 </script>
 <style scoped lang='less'>
 .abb{
-    background-image: url(../assets/img/Second.jpg);
+    background-image: url(../assets/img/Second/2.1backgroud.png);
     width: 100vw;
     height:100vh;
     position: absolute;
@@ -19,4 +34,206 @@ import { reactive } from 'vue';
     background-size: cover;
 
 }
+.text1-1 {
+  width: 105px;
+  height: 15px;
+  margin-left: 136px;
+  margin-top: 166px;
+  position: absolute;
+  animation: fadeInUp 1s  ease;
+  animation-delay: 0.5s;
+  animation-fill-mode: both; //动画开始前隐藏
+}
+.text1-2 {
+  width: 113px;
+  height: 13px;
+  margin-left: 132px;
+  margin-top: 200px;
+  position: absolute;
+  animation: fadeInUp 1s ease;
+  animation-delay: 1.5s;
+  animation-fill-mode: both; //动画开始前隐藏
+}
+.text1-3 {
+  width: 150px;
+  height: 13px;
+  margin-left: 114px;
+  margin-top: 235px;
+  position: absolute;
+  animation: fadeInUp 1s ease;
+  animation-delay: 2.5s;
+  animation-fill-mode: both; //动画开始前隐藏
+}
+.text1-4 {
+  width: 175px;
+  height: 13px;
+  margin-left: 101px;
+  margin-top: 269px;
+  position: absolute;
+  animation: fadeInUp 1s ease;
+  animation-delay: 3.5s;
+  animation-fill-mode: both; //动画开始前隐藏
+}
+.text1-5 {
+  width: 163px;
+  height: 13px;
+  margin-left: 107px;
+  margin-top: 303px;
+  position: absolute;
+  animation: fadeInUp 1s ease;
+  animation-delay: 4.5s;
+  animation-fill-mode: both; //动画开始前隐藏
+}
+.text1-6 {
+  width: 163px;
+  height: 13px;
+  margin-left: 107px;
+  margin-top: 338px;
+  position: absolute;
+  animation: fadeInUp 1s ease;
+  animation-delay: 5.5s;
+  animation-fill-mode: both; //动画开始前隐藏
+}
+.text1-7 {
+  width: 215px;
+  height: 13px;
+  margin-left: 81px;
+  margin-top: 372px;
+  position: absolute;
+  animation: fadeInUp 1s ease;
+  animation-delay: 6.5s;
+  animation-fill-mode: both; //动画开始前隐藏
+}
+.text1-8 {
+  width: 104px;
+  height: 14px;
+  margin-left: 138px;
+  margin-top: 406px;
+  position: absolute;
+  animation: fadeInUp 1s ease;
+  animation-delay: 7.5s;
+  animation-fill-mode: both; //动画开始前隐藏
+}
+.text1-9 {
+  width: 213px;
+  height: 13px;
+  margin-left: 82px;
+  margin-top: 441px;
+  position: absolute;
+  animation: fadeInUp 1s ease;
+  animation-delay: 8.5s;
+  animation-fill-mode: both; //动画开始前隐藏
+}
+.text1-10 {
+  width: 139px;
+  height: 14px;
+  margin-left: 119px;
+  margin-top: 475px;
+  position: absolute;
+  animation: fadeInUp 1s ease;
+  animation-delay: 9.5s;
+  animation-fill-mode: both; //动画开始前隐藏
+}
+.liu1-1 {
+  width: 52px;
+  height: 77px;
+  margin-left: 100px;
+  margin-top: -100px;
+  position: absolute;
+  animation: liu1 3.5s linear infinite;
+  animation-delay: 1s;
+  animation-fill-mode: both;
+}
+.liu1-2 {
+  width: 52px;
+  height: 77px;
+  margin-left: 200px;
+  margin-top: -100px;
+  position: absolute;
+  animation: liu1 4s linear infinite;
+  animation-delay: 2s;
+  animation-fill-mode: both;
+}
+
+.liu1-3 {
+  width: 52px;
+  height: 77px;
+  margin-left: 300px;
+  margin-top: -100px;
+  position: absolute;
+  animation: liu1 4.9s linear infinite;
+  animation-delay: 3s;
+  animation-fill-mode: both;
+}
+.liu2-1 {
+  width: 87px;
+  height: 125px;
+  margin-left: 70px;
+  margin-top: -100px;
+  position: absolute;
+  animation: liu2 3.5s linear infinite;
+  animation-delay: 0.5s;
+  animation-fill-mode: both;
+}
+.liu2-2 {
+  width: 87px;
+  height: 125px;
+  margin-left: 150px;
+  margin-top: -100px;
+  position: absolute;
+  animation: liu2 2.8s linear infinite;
+  animation-delay: 1.5s;
+  animation-fill-mode: both;
+}
+.liu2-3 {
+  width: 87px;
+  height: 125px;
+  margin-left: 250px;
+  margin-top: -100px;
+  position: absolute;
+  animation: liu2 2.5s linear infinite;
+  animation-delay: 2.5s;
+  animation-fill-mode: both;
+}
+
+@keyframes liu1 {
+  from {
+    opacity: 1;
+    -webkit-transform: translate(100px, -100px);
+    transform: translate3d(100px, -100px);
+  }
+
+  to {
+    opacity: 1;
+    -webkit-transform: translate(-300px, 800px);
+    transform: translate(-300px, 800px);
+  }
+}
+@keyframes liu2 {
+  from {
+    opacity: 1;
+    -webkit-transform: translate(250px, -100px);
+    transform: translate3d(250px, -100px);
+  }
+
+  to {
+    opacity: 1;
+    -webkit-transform: translate(-100px, 800px);
+    transform: translate(-100px, 800px);
+  }
+}
+@keyframes fadeInUp {
+  from {
+    opacity: 0;
+    -webkit-transform: translate3d(0, 15px, 0);
+    transform: translate3d(0, 15px, 0);
+  }
+
+  to {
+    opacity: 1;
+    -webkit-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+  }
+}
+
 </style>
